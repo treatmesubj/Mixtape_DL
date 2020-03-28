2a176e3956e756512958f24fa033827c558d139c
John Hupperts <39680353+treatmesubj@users.noreply.github.com>
2020-03-28 12:03:55 -0500

    No HTML compacting spaces

--- datpif_dl.py
+++ datpif_dl.py
@@ -4,8 +4,8 @@ import os
 import re
 from pprint import pprint
 
-# link = input("datpiff mixtape link: ")
-link = "https://www.datpiff.com/Big-Sean-Detroit-mixtape.390127.html"
+print("https://www.datpiff.com/Big-Sean-Detroit-mixtape.390127.html")
+link = input("datpiff mixtape link: ")
 
 user_page = HTML(html=requests.get(link).text)
 artist = user_page.find("li.artist", first=True).text
@@ -17,17 +17,19 @@ if not os.path.exists(dir_loc):
 
 mixtape_code = link.split(".")[-2]
 player_link = f"https://embeds.datpiff.com/mixtape/{mixtape_code}"
-player_html = HTML(html=requests.get(player_link).text)
+# player_html = HTML(html=requests.get(player_link).text)
 
-js = player_html.find("body>script")[-1].text
-pprint(js)
+# js = player_html.find("body>script")[-1].text
+# pprint(js)
 
-mfile_starts = [i.end() for i in re.finditer("mfile\":trackPrefix.concat", js)]
-mfile_ends = [i.end() for i in re.finditer(".mp3", js)][1:]
+pl = requests.get(player_link).text
+
+mfile_starts = [i.end() for i in re.finditer("mfile\":trackPrefix.concat", pl)]
+mfile_ends = [i.end() for i in re.finditer(".mp3", pl)][1:]
 mfiles = dict(zip(mfile_starts, mfile_ends))
 
-song_links = [js[key+3:val] for key,val in mfiles.items()]
-pprint(song_links)
+song_links = [pl[key+3:val] for key,val in mfiles.items()]
+
 
 def substr(string, start_substr_substr, end_substr_substr):
 	substr_start_index = string.find(start_substr_substr)
@@ -35,21 +37,15 @@ def substr(string, start_substr_substr, end_substr_substr):
 	return string[substr_start_index:substr_end_index]
 
 
-base_url = substr(js, "var trackPrefix", ";").split("'")[1]
+base_url = substr(pl, "var trackPrefix", ";").split("'")[1]
 
 for song_link in song_links:
-	try:
 
-		full_link = f"{base_url}{song_link}"
-		print(full_link)
+	full_link = f"{base_url}{song_link}"
+	print(full_link)
 
-		with open(f"{dir_loc}\\{song_link}", "wb") as f:
-			f.write(requests.get(f"{base_url}{song_link}").content)
+	with open(f"{dir_loc}\\{song_link}", "wb") as f:
+		f.write(requests.get(f"{full_link}").content)
 
-	except Exception as e:
-		print(e)
-		breakpoint()
 
 input("done")
-
-playerData.tracks[0].mfile
